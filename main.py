043e7fead323f4cfcc7cee104a4231767a8c7b0c
Saúl  Expósito Jiménez <38258443-saulexposito@users.noreply.replit.com>
2024-12-08 18:52:30 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Definimos las variables
+a = 5
+b = 3
+
+# Comparamos las variables
+if a > b:
+   print(f"La variable 'a' ({a}) es mayor que la variable 'b' ({b}).")
+elif b > a:
+   print(f"La variable 'b' ({b}) es mayor que la variable 'a' ({a}).")
+else:
+   print("Ambas variables son iguales.")
+
